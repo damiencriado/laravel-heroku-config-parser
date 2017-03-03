ed2540d0cdcbefe8625577e7a3a3ff20a3e31034
itsDamien <its@damien.is>
2017-03-03 10:12:33 +0100

    add travis + coveralls

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ItsDamien\Heroku\Tests;
+
+class ExampleTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAssertIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
